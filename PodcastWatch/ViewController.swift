6e1f626ce0d21c9c4b6d5a0c70953ccb691fdc47
Samantha John <sam@gethopscotch.com>
2015-12-24 13:23:22 -0500

    Initial commit

---
+++ PodcastWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodcastWatch
+//
+//  Created by Samantha John on 12/24/15.
+//  Copyright © 2015 SamanthaJohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
